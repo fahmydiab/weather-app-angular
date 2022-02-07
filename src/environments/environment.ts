2521aaeb982aedf24cc8b59255e0efb5d55dc6e6
fahmydiab <king.diab.007@gmail.com>
2022-02-07 22:40:53 +0200

    add apiURL and api Key to environments files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://api.openweathermap.org/data/2.5/',
+  apiKey: '<Your API key>'
 };
 
 /*
@@ -14,3 +16,4 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
